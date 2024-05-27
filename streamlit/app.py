5f14c0c6699f08739553b28a3bec6fd1157772cd
ArdaKaymaz <146623362+ArdaKaymaz@users.noreply.github.com>
2024-05-27 23:35:29 +0300

    Update

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,28 +1,36 @@
-# Application Developing Project with Generative AI
-
-# 1 Streamlit Library
-
 import streamlit as st
+import keras
+import tensorflow as tf
+from PIL import Image
+import numpy as np
+import os
 
-# 2 Start Streamlit
+model = tf.keras.models.load_model("streamlit/cat_dog_clf_model.h5")
 
-# st.write("Hello World")
-
-
-# 3 Setting Page Configurations
-
+# Set the page configuration
 st.set_page_config(page_title="Cat-Dog Classifier", page_icon="🔎")
 
+# Add a header and description
 st.header("Hello, my name is Classifier!")
 st.divider()
-st.write("I'm here to assist you to classify if it is a dog:dog2: or a cat:cat2:!")
+st.write("I'm here to assist you to classify if it is a dog 🐶 or a cat 🐱!")
+
+# Create a file uploader widget
+uploaded_image = st.file_uploader(label="Click to upload image", type=["jpg", "jpeg", "png"])
 
 
-# st.text_input(label="Your email:")
-# st.text_input(label="Your password:", type="password")
-# st.number_input(label="Your age", min_value=0, max_value=100, value=24)
-# st.slider(label="Your age", min_value=0, max_value=100, value=24)
-# st.checkbox(label="Remember me")
-# st.radio(label="What is your status?", options=["student", "employed", "unemployed"])
-# st.button(label="Login")
-# st.file_uploader(label="Click to upload image")
+if uploaded_image is not None:
+    image = Image.open(uploaded_image)
+    
+    # Display the image with a caption
+    st.image(image, caption='Uploaded Image', use_column_width=True)
+    
+    # Optionally, display additional information about the image
+    # st.write(f"Image format: {image.format}")
+    # st.write(f"Image size: {image.size}")
+    # st.write(f"Image mode: {image.mode}")
+
+else:
+    st.write("Please upload an image file.")
+
+pred_but = st.button(label="Predict")
